2fa67f08ce730d2c3e152229e419f3531e1bca45
marishash <marisha@thebrightly.co>
2021-03-01 20:23:36 -0800

    Renamed test file to APIWebServerApplicationTests

--- src/main/java/com/stellar/apiwebserver/SnippetNotFoundException.java
+++ src/main/java/com/stellar/apiwebserver/SnippetNotFoundException.java
@@ -3,6 +3,6 @@ package com.stellar.apiwebserver;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "video not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "snippet not found")
 public class SnippetNotFoundException extends RuntimeException{
 }
--- src/test/java/com/stellar/apiwebserver/APIWebServerApplicationTests.java
+++ src/test/java/com/stellar/apiwebserver/APIWebServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.stellar.restservice;
+package com.stellar.apiwebserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestServiceApplicationTests {
+class APIWebServerApplicationTests {
 
 	@Test
 	void contextLoads() {
